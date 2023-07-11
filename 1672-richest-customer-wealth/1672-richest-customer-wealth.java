551b9a3d8d1cfd671f8affc22d0371921fdb9a73
Tausiq Samantaray <tausiqsamantaray03@gmail.com>
2023-07-11 15:25:42 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (29.35%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i<accounts.length;i++){
+            int sum = 0;
+            for(int j = 0; j < accounts[i].length;j++){
+                sum += accounts[i][j];
+            }
+            if(sum > max){
+                max = sum;
+            }
+        }
+        
+        return max;
+        
+        
+    }
+}
