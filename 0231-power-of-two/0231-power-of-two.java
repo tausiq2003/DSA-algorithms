4da6bc3596fa14f4fa3aaa6ba70ef0b54dfc83fa
Tausiq Samantaray <tausiqsamantaray03@gmail.com>
2023-09-05 23:18:58 +0530

    Time: 1 ms (90.70%), Space: 39.1 MB (96.79%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -2,16 +2,34 @@ class Solution {
     public boolean isPowerOfTwo(int n) {
      // there exists an integer x such that x == 2^x
         //use recursion
+//         if (n <= 0){
+//             return false;
+//         }
+//         if (n < 2){
+//             return true;
+
+//         }
+//         if(n % 2 == 1){
+//             return false;
+//         }
+//         return isPowerOfTwo(n/2);
+        // this is an brilliant solution but look at return types yeah that pure luck
+        // System.out.println(recursive(n));
+        return recursive(n) % 2 == 0;
+        
+    }
+    public int recursive(int n){
         if (n <= 0){
-            return false;
+            return 1;
         }
         if (n < 2){
-            return true;
+            return 0;
 
         }
         if(n % 2 == 1){
-            return false;
+            return 1;
         }
-        return isPowerOfTwo(n/2);
-    }
+        return recursive(n/2);
+    
+}
 }
