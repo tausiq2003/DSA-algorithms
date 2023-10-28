be5439f7dad5f3ad877b8871018f0077326a786f
Tausiq Samantaray <tausiqsamantaray03@gmail.com>
2023-10-28 23:27:04 +0530

    Time: 2 ms (67.88%), Space: 48.5 MB (41.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,36 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        return insertionSort(nums);
-    }
-    public int insertionSort(int[] nums){
-        for (int i = 0; i < nums.length-1; i++){
-            for (int j = i+1; j > 0; j--){
-                if (nums[j] < nums[j-1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j-1];
-                    nums[j-1] = temp;
-            
-                }
-                else{
-                    break;
-                }
-            }
-        }
+        quickSort(nums, 0, nums.length-1);
         return nums[(nums.length-1)/2];
+    }
+    public void quickSort(int[] nums, int low, int hi){
+        if (low >= hi){
+            return;
+        }
+        int s = low;
+        int e = hi;
+        int mid = s + (e-s)/2;
+        int pivot = nums[mid];
+        //putting the pivot at correct index
+        while(s <= e){
+        while (nums[s] < pivot){
+            s++;
+        }
+        while(nums[e] > pivot){
+            e--;
+        }
+        if (s <= e){
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+            s++;
+            e--;
+        }
+        }
+        quickSort(nums, low, e);
+        quickSort(nums, s, hi);
+        
+        
+    }
 
     }
-}
